ea5884767fd6e2b23d0a01e372a7b36d62e31da9
396640195 <396640195@qq.com>
2015-04-27 20:56:04 +0800

    delete all files

--- LitePalApp/LitePalApp/app/src/main/java/demo/litepal/com/litepalapp/DataSuportDao.java
+++
@@ -1,9 +0,0 @@
-package demo.litepal.com.litepalapp;
-
-/**
- * Created by Administrator on 2015/4/27 0027.
- */
-public interface DataSuportDao {
-    public  int delete();
-    public boolean save();
-}
